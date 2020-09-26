9f5f3dfc7b78d451442829cf141d677984cf3a61
James Brown <bjames1@vt.edu>
2020-09-26 13:32:29 -0400

    Update gabor_filt.py

--- gabor_filt.py
+++ gabor_filt.py
@@ -3,18 +3,6 @@ Author: James Michael Brown
 Date: 09/24/2020
 Note: Gabor edge detection code adapted for Pythonista, Python 3.6.
 
-
-"Pythonista is a complete development environment for writing Python™ 
-scripts on your iPad or iPhone. Lots of examples are included — from games 
-and animations to plotting, image manipulation, custom user interfaces, 
-and automation scripts.
-
-In addition to the powerful standard library, Pythonista provides extensive
-support for interacting with native iOS features, like contacts, reminders,
-photos, location data, and more."
-
-http://omz-software.com/pythonista/
-
 """
 
 # # # Import Modules
